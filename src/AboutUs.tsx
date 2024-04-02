bfc36eadd290783556b5a9559a629905218d3c48
= <=>
2024-04-02 12:48:18 +0530

    First commit

--- src/AboutUs.tsx
+++ src/AboutUs.tsx
@@ -3,7 +3,7 @@ export default function AboutUs() {
         <div className="flex justify-center items-center gap-20 py-20 px-40">
             <div className="">
                 <img src="/image1.png" className="h-96" alt="" />
-            </div>
+            </div> 
 
             <div className="w-full md:w-1/2">
                 <p className="text-xl text-stone-500">Short names can be catchy and memorable, making them effective for branding and marketing purposes. Make sure to choose a name that aligns with your company's identity and goals.</p>
